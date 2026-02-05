b6dc57d78744dd522e60b6b28e2fa1f20a679aa1
mughazi <fikrigha@gmail.com>
2026-02-04 22:44:12 -0500

    remove export

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "",
-  images: {
-    unoptimized: true,
-  },
   // Turbopack configuration for Next.js 16
   turbopack: {
     resolveAlias: {
